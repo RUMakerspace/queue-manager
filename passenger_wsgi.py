59ca4d65b1c945b1f7c5cb5485e2706eace450f3
Tim Flynn <tjflynn@theflynn.org>
2020-12-15 16:00:18 -0500

    basic subjob parsing now working?

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -8,10 +8,10 @@ if sys.version < "2.4":
 
 # Change before deploy.
 try:
-	with open('debug_run.py') as w:
-		debug = True
+    with open("debug_run.py") as w:
+        debug = True
 except:
-	debug=False
+    debug = False
 
 if not debug:
     INTERP = os.path.join(os.environ["HOME"], "frcregs.com", "bin", "python")
@@ -23,45 +23,54 @@ from flask import Flask, render_template, redirect, url_for, jsonify, request
 import flask
 
 application = Flask(__name__)
-application.secret_key = open('supersecret.key').read()
+application.secret_key = open("supersecret.key").read()
 
 printDB = "prints.json"
 
+
 def getPrints(howMany=20):
-	printsJSON = json.loads(open(printDB).read())
-	print(printsJSON)
-	
-	printsJSON = sorted(printsJSON, key = lambda x: (x['unixTime'], x['hash']), reverse=True)
-	
-	return printsJSON[:howMany]
+    printsJSON = json.loads(open(printDB).read())
+    print(printsJSON)
+
+    printsJSON = sorted(
+        printsJSON, key=lambda x: (x["unixTime"], x["hash"]), reverse=True
+    )
+
+    return printsJSON[:howMany]
+
 
 def makePrintDB():
-	with open(printDB,'w') as prints:
-		prints.write(json.dumps([]))
-		
+    with open(printDB, "w") as prints:
+        prints.write(json.dumps([]))
+
+
 def addPrint(printData):
-	printJSON = json.loads(open(printDB).read())
-	import time
-	import hashlib
-	printData['unixTime'] = time.time()
-	printData['hash'] = str(hashlib.md5(str.encode(str(printData))).hexdigest())
-	
-	printJSON.append(printData)
-	
-	with open(printDB,'w') as x:
-		x.write(json.dumps(printJSON))
-		
+    printJSON = json.loads(open(printDB).read())
+    import time
+    import hashlib
+
+    printData["unixTime"] = time.time()
+    printData["hash"] = str(hashlib.md5(str.encode(str(printData))).hexdigest())
+
+    printJSON.append(printData)
+
+    with open(printDB, "w") as x:
+        x.write(json.dumps(printJSON))
+
+
 ### LOGIN SHIT
 import flask_login
-from flask_login import LoginManager, UserMixin, \
-                                login_required, login_user, logout_user
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+
 login_manager = flask_login.LoginManager()
 login_manager.init_app(application)
 
-users = json.loads(open('userdb.json').read())
+users = json.loads(open("userdb.json").read())
+
 
 class User(flask_login.UserMixin):
     pass
+
 
 @application.before_request
 def before_request():
@@ -82,7 +91,7 @@ def user_loader(email):
 
 @login_manager.request_loader
 def request_loader(request):
-    email = request.form.get('email')
+    email = request.form.get("email")
     if email not in users:
         return
 
@@ -91,42 +100,45 @@ def request_loader(request):
 
     # DO NOT ever store passwords in plaintext and always compare password
     # hashes using constant-time comparison!
-    user.is_authenticated = request.form['password'] == users[email]['password']
+    user.is_authenticated = request.form["password"] == users[email]["password"]
 
     return user
-    
-@application.route('/login', methods=['GET', 'POST'])
+
+
+@application.route("/login", methods=["GET", "POST"])
 def login():
     # Here we use a class of some kind to represent and validate our
     # client-side form data. For example, WTForms is a library that will
     # handle this for us, and we use a custom LoginForm to validate.
-    if request.method == 'GET':
-        return flask.render_template('pw.html')
-    if request.method == 'POST':
+    if request.method == "GET":
+        return flask.render_template("pw.html")
+    if request.method == "POST":
         # Login and validate the user.
         # user should be an instance of your `User` class
-        if 'email' not in flask.request.form:
-            flask.redirect(flask.url_for('index'))
-        email = flask.request.form['email']
+        if "email" not in flask.request.form:
+            flask.redirect(flask.url_for("index"))
+        email = flask.request.form["email"]
         try:
-            if flask.request.form['password'] == users[email]['password']:
+            if flask.request.form["password"] == users[email]["password"]:
                 user = User()
                 user.id = email
                 login_user(user)
 
         except:
-            flask.redirect(url_for('logout'))
+            flask.redirect(url_for("logout"))
         # is_safe_url should check if the url is safe for redirects.
         # See http://flask.pocoo.org/snippets/62/ for an example.
 
-        return flask.redirect(flask.url_for('index'))
-    
+        return flask.redirect(flask.url_for("index"))
+
+
 @application.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
-    
+    return redirect(url_for("index"))
+
+
 @login_manager.user_loader
 def user_loader(email):
     if email not in users:
@@ -135,52 +147,107 @@ def user_loader(email):
     user = User()
     user.id = email
     return user
-    
-@application.route('/login',methods=['POST','GET'])
+
+
+@application.route("/login", methods=["POST", "GET"])
 def loginHelper():
-    if request.method == 'POST':
-        passphrase = request.form['passphrase']     
+    if request.method == "POST":
+        passphrase = request.form["passphrase"]
         if passphrase in passwords2:
             id = passphrase
             user = User()
             login_user(user)
-    if request.method == 'GET':
-        return render_template('pw.html')
-    #print(request.values)     
-    return redirect(url_for('index'))
+    if request.method == "GET":
+        return render_template("pw.html")
+    # print(request.values)
+    return redirect(url_for("index"))
 
-@application.route('/')    
+
+@application.route("/")
 def indexPage():
-	#makePrintDB()
-	return render_template('main.html', prints=getPrints(5))
-	
-@application.route('/add', methods=['GET','POST'])    
-def addPage():
-	if request.method == "POST":
-		from pprint import pprint
-		pprint(request.form.to_dict())
-		addPrint(request.form.to_dict())
-	return render_template('add.html')
-	
-def hasSubprints(request):
-	hasSubjobs = False
-	numSubjobs = 0
-	derivedKeys = request.keys()
-	for key in derivedKeys:
-		if "subjob" in key:
-			hasSubjobs = True
-			if int(key.split("_")[2])  > numSubjobs:
-				numSubjobs = int(key.split("_")[2])
-				
-	return [hasSubjobs, numSubjobs]
-@application.route('/addbulk', methods=['GET','POST'])    
-def addBulkPage():
-	if request.method == "POST":
-		tempData = request.form.to_dict()
-		from pprint import pprint
+    # makePrintDB()
+    return render_template("main.html", prints=getPrints(5))
 
-		hasSubjobs, numSubjobs = hasSubprints(request.form.to_dict())
-		
-		print(hasSubjobs)
-		print(numSubjobs)
-	return render_template('addbulk.html')
+
+@application.route("/add", methods=["GET", "POST"])
+def addPage():
+    if request.method == "POST":
+        from pprint import pprint
+
+        pprint(request.form.to_dict())
+        addPrint(request.form.to_dict())
+    return render_template("add.html")
+
+
+# XXX TODO
+# Implement this page's logic.
+@application.route("/manage/<hash>", methods=["GET", "POST"])
+def managePrint(hash):
+    if request.method == "POST":
+        print("Post bois")
+    return render_template("add.html")
+
+
+def hasSubprints(request):
+    # This function takes in a request and knowing the format
+    # of the POST we can break it apart.
+    # The first half is for getting the number of subprints in the bulk post and whether we have them or not.
+    hasSubjobs = False
+    numSubjobs = 0
+    derivedKeys = request.keys()
+    for key in derivedKeys:
+        if "subjob" in key:
+            hasSubjobs = True
+            if int(key.split("_")[2]) > numSubjobs:
+                numSubjobs = int(key.split("_")[2])
+
+    # the sectiond portion specifically breaks apart the flat structure
+    # into a list of subjob keys we can easily iterate over and replace
+    # components of the parent job with.
+    subjobCollected = {}
+
+    for key in derivedKeys:
+        if "subjob" in key:
+            index = int(key.split("_")[2])
+            if index not in subjobCollected:
+                subjobCollected[
+                    index
+                ] = {}  # if the subjob number isn't in the temporary list, we add it.
+            subjobCollected[index][key] = request[
+                key
+            ]  # request as added back in is just the k/v as a dict.
+    from pprint import pprint
+
+    pprint(subjobCollected)
+    return [hasSubjobs, numSubjobs, subjobCollected]
+
+
+@application.route("/addbulk", methods=["GET", "POST"])
+def addBulkPage():
+    if request.method == "POST":
+        tempData = request.form.to_dict()
+        from pprint import pprint
+
+        hasSubjobs, numSubjobs, sj = hasSubprints(request.form.to_dict())
+        addPrint(tempData)
+        if numSubjobs > 0:
+            # make a temp request, clear out any subjob keys
+            # then append the keys as a subdict, then iterate to add them.
+            import copy
+
+            placeholderReq = copy.deepcopy(request.form.to_dict())
+            placeholderKeys = placeholderReq.keys()
+            for x in list(placeholderKeys):
+                if "subjob" in x:
+                    placeholderReq.pop(x, None)
+
+            for subjob in sj.keys():
+                temp = placeholderReq
+                temp["parentJobName"] = sj[subjob][str("subjob_name_" + str(subjob))]
+                temp["parentNotesBox"] = sj[subjob][str("subjob_notes_" + str(subjob))]
+                temp["printTime"] = sj[subjob][str("subjob_printtime_" + str(subjob))]
+                temp["location"] = sj[subjob][str("subjob_location_" + str(subjob))]
+
+                addPrint(temp)
+
+    return render_template("addbulk.html")
