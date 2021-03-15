72f8d1f82193b849ddf867810bb7de5b8fa7277d
Jared Tulayan <2lyNJ.E@gmail.com>
2021-03-15 17:19:49 -0400

    Implement adding prints, begin edit prints

--- model/db.py
+++ model/db.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import timedelta
 
 db = SQLAlchemy()
 
@@ -9,8 +10,13 @@ class PrintJob(db.Model):
     netid = db.Column(db.String(7), unique=False, nullable=True)
     email = db.Column(db.String(120), unique=False, nullable=True)
     customer_name = db.Column(db.String(80), unique=False, nullable=True)
-    length = db.Column(db.Time, unique=False, nullable=False)
+    length = db.Column(db.Interval, unique=False, nullable=False)
     status = db.Column(db.String(10), unique=False, nullable=False)
+    description = db.Column(db.Text, unique=False, nullable=True)
+
+    # Test
+    def __init__(self, job_name=None, author="Makerspace", netid=None, email=None, customer_name=None, length=None, status="NEW"):
+        super().__init__(job_name=job_name, author=author, netid=netid, email=email, customer_name=customer_name, length=length, status=status)
 
     def __repr__(self):
-        return f"{self.name}: {self.length}"
+        return f"{self.id}: {self.job_name}"
--- views/prints.py
+++ views/prints.py
@@ -1,5 +1,6 @@
 from flask import current_app, Blueprint, render_template, redirect, request, url_for
-from model.db import PrintJob
+from datetime import timedelta
+from model.db import PrintJob, db
 
 prints = Blueprint('prints', __name__, url_prefix='/prints')
 
@@ -12,8 +13,21 @@ def add_print():
     if request.method == "GET":
         return render_template("prints/add.html")
     elif request.method == "POST":
+        # TODO: Add author to printjob
         job_name = request.form.get("job_name")
-        print(f"{job_name}")
+        netid = request.form.get("netid")
+        length = timedelta(
+            days=int(request.form.get("l_days")),
+            hours=int(request.form.get("l_hours")),
+            minutes=int(request.form.get("l_mins")))
+
+        db.session.add(PrintJob(
+            job_name=job_name, 
+            netid=netid, 
+            length=length))
+
+        db.session.commit()
+
         return redirect(url_for("prints.show_prints"))
     else:
         return "Invalid method!"
@@ -25,7 +39,8 @@ def remove_print(id):
 @prints.route('/edit/<id>', methods=["GET", "POST"])
 def edit_print(id):
     if request.method == "GET":
-        pass
+        print = PrintJob.query.get(id)
+        return render_template("prints/edit.html", print=print)
     elif request.method == "POST":
         pass
     else:
