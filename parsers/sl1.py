d7a822be04bbf50955eb46a14ae1109d878d6ba8
Flynn <tjflynn@theflynn.org>
2021-11-07 20:46:01 -0500

    Added sl1 parser.

---
+++ parsers/sl1.py
@@ -0,0 +1,6 @@
+#SL1 File Notes (PrusaSlicer)
+#A ZIP of thumnail exposure jpgs.
+# config.ini
+#    printTime = 7811.000000
+#    materialName = Prusa Orange Tough @0.025
+#    usedMaterial = 2.740441
